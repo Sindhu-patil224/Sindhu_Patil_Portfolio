851bb18c90d54c61e408723a7050da776ff915a5
Sindhu-patil224 <sindhu.patil224@gmail.com>
2026-08-02 13:59:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,64 @@
+// Smooth Scroll
 
+document.querySelectorAll('nav a').forEach(anchor => {
+
+anchor.addEventListener('click', function(e){
+
+e.preventDefault();
+
+document.querySelector(this.getAttribute('href')).scrollIntoView({
+
+behavior:'smooth'
+
+});
+
+});
+
+});
+
+
+// Highlight Active Navbar
+
+const sections=document.querySelectorAll("section");
+const navLinks=document.querySelectorAll("nav ul li a");
+
+window.addEventListener("scroll",()=>{
+
+let current="";
+
+sections.forEach(section=>{
+
+const sectionTop=section.offsetTop-150;
+
+if(scrollY>=sectionTop){
+
+current=section.getAttribute("id");
+
+}
+
+});
+
+navLinks.forEach(link=>{
+
+link.classList.remove("active");
+
+if(link.getAttribute("href")==="#"+current){
+
+link.classList.add("active");
+
+}
+
+});
+
+});
+
+
+// Navbar Shadow
+
+window.addEventListener("scroll",()=>{
+
+const header=document.querySelector("header");
+
+header.classList.toggle("sticky",window.scrollY>50);
+
+});
